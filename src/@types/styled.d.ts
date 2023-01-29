3b2363e499be1d5721fdc9b19b6ba79bcf962cda
mariafantuci <mariafantuci@gmail.com>
2023-01-28 21:03:25 -0300

    Create styles type and fix base style

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,10 @@
+// algumacoisa.d.ts são arquivos de definição só tem tipagem
+
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
