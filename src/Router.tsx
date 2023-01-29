895c2ccd84e748aa9a2246d42ef20cf906f3db6b
mariafantuci <mariafantuci@gmail.com>
2023-01-28 22:38:29 -0300

    Change the header on the route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route} from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout/Index'
 import { Home } from './pages/Home'
 import { Login } from './pages/Login'
 import { Post } from './pages/Post'
@@ -6,7 +7,7 @@ import { Post } from './pages/Post'
 export function Router() {
   return (
     <Routes>
-        <Route path="/" element={<Home />}>
+        <Route path="/" element={<DefaultLayout />}>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/Post" element={<Post />} />
