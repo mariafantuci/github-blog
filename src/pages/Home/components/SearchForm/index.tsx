d7fdb0d3a7a86bd938eb4b668cb7c432836eabe2
mariafantuci <mariafantuci@gmail.com>
2023-02-01 22:17:42 -0300

    start the searchform logic

--- src/pages/Home/components/SearchForm/index.tsx
+++ src/pages/Home/components/SearchForm/index.tsx
@@ -1,13 +1,39 @@
+/* eslint-disable prettier/prettier */
 import { SearchFormContainer } from './style'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
 
-export function SearchForm() {
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
+interface SearchFormProps{
+  getRespo: (query?: string) => Promise<void>
+}
+export function SearchForm({ getRespo }: SearchFormProps) {
+  const {
+    register,
+    handleSubmit,
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  async function handleSearchIssues(data: SearchFormInputs) {
+    await getRespo(data.query)
+  }
   return (
-    <SearchFormContainer>
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchIssues)}>
       <div>
         <h2>Publicações</h2>
         <span>6 publicações</span>
       </div>
-      <input type="text" placeholder="Buscar conteúdo" />
+      <input 
+        type="text" 
+        placeholder="Buscar conteúdo" 
+        {...register('query')}
+      />
     </SearchFormContainer>
   )
 }
