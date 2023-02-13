2cc50d745f5af38fda7d5f4cdcab6083079e7f2a
mariafantuci <mariafantuci@gmail.com>
2023-02-13 18:22:43 -0300

    create a logic to not display the profile content if the object is empty

--- src/pages/Home/components/Profile/index.tsx
+++ src/pages/Home/components/Profile/index.tsx
@@ -47,47 +47,53 @@ export function Profile() {
   useEffect(() => {
     getProfile(userName)
   }, [getProfile]);
-
+  console.log('profile',profile)
   return (
-    <ProfileContainer>
-      <ProfileContainerImg>
-        <img
-          src={profile.avatar_url}
-          alt="github avatar image"
-        />
-      </ProfileContainerImg>
+    <>
+    {
+      Object.keys(profile).length > 0 &&
+        <ProfileContainer>
+          <ProfileContainerImg>
+            <img
+              src={profile.avatar_url}
+              alt="github avatar image"
+            />
+          </ProfileContainerImg>
 
-      <ProfileContent>
-        <ProfileContentText>
-          <h1>{profile.name}</h1>
-          <a href={profile.html_url} title={profile.name + ' profile'}>
-            <ProfileGit>
-              <span>github</span>
-                <img src={arrow} alt="arrow up" />
-            </ProfileGit>
-          </a>
-          </ProfileContentText>
-        <p>
-          {profile.bio}
-        </p>
-        <ProfileItemContainer>
-          <ProfileItem>
-            <img src={github} alt="github icon" />
-            <span>{profile.login}</span>
-          </ProfileItem>
-          {profile.company && 
-            <ProfileItem>
-            <img src={building} alt="building icon" />
-            <span>{profile.company}</span>
-            </ProfileItem>
-          }
-          
-          <ProfileItem>
-            <img src={user} alt="user icon" />
-            <span>{profile.followers} seguidores</span>
-          </ProfileItem>
-        </ProfileItemContainer>
-      </ProfileContent>
-    </ProfileContainer>
+          <ProfileContent>
+            <ProfileContentText>
+              <h1>{profile.name}</h1>
+              <a href={profile.html_url} title={profile.name + ' profile'}>
+                <ProfileGit>
+                  <span>github</span>
+                    <img src={arrow} alt="arrow up" />
+                </ProfileGit>
+              </a>
+              </ProfileContentText>
+            <p>
+              {profile.bio}
+            </p>
+            <ProfileItemContainer>
+              <ProfileItem>
+                <img src={github} alt="github icon" />
+                <span>{profile.login}</span>
+              </ProfileItem>
+              {profile.company && 
+                <ProfileItem>
+                <img src={building} alt="building icon" />
+                <span>{profile.company}</span>
+                </ProfileItem>
+              }
+              
+              <ProfileItem>
+                <img src={user} alt="user icon" />
+                <span>{profile.followers} seguidores</span>
+              </ProfileItem>
+            </ProfileItemContainer>
+          </ProfileContent>
+        </ProfileContainer>
+    }
+    </>
+  
   )
 }
